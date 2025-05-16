80f768fd3eea5d585e0a6d113c0aadba9fb71d75
Mai Nhan Kiet <mainhankietthcstb@gmail.com>
2025-05-16 11:23:05 +0700

    create basic structure project be

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express' //import express vào dự án
+const app = express() //dùng express tạo 1 server
+const port = 3000 //server sẽ chạy trên cổng port 3000
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(port, () => {
+  console.log(`Project này đang chạy trên post ${port}`)
+})
